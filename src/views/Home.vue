aa87f85716ad57716d376739b4bde0bb9c15f39f
Waclaw Bielecki <hardhill@mail.ru>
2019-11-02 01:27:59 +0800

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="home">
-    
-    <h2>Home page</h2>
+     <canvas id="game" width="640px" height="400px"/>
   </div>
 </template>
 
@@ -11,8 +10,34 @@
 
 export default {
   name: 'home',
-  components: {
+  data:function(){
+    return {
+      ctx:{}
+    }
+  },
+  calculated:{
     
+    
+  },
+  created:()=>{
+    this.Init()
+  },
+  methods:{
+    Init(){
+      var ctx = document.getElementById("game").getContext("2d")
+      var bg = new Image()
+      //var bird = new Image()
+      bg.src = "@assets/logo.png"
+      ctx.drawImage(bg,0,0)
+    }
   }
+
+  
 }
 </script>
+<style scoped>
+.home{
+  display: block;
+  text-align: center;
+}
+</style>
